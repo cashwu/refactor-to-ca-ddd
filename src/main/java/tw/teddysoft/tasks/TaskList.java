751e19931178598b7a4cf7b5ad913579bb32e1db
Teddy <teddy.chen.tw@gmail.com>
2024-04-17 08:39:53 +0800

    [Step 3-4: Extract Class from Private Method] Extract TaskList::help to Help class

--- src/main/java/tw/teddysoft/tasks/TaskList.java
+++ src/main/java/tw/teddysoft/tasks/TaskList.java
@@ -63,7 +63,7 @@ public final class TaskList implements Runnable {
                 uncheck(commandRest[1]);
                 break;
             case "help":
-                help();
+                new Help(out).help();
                 break;
             default:
                 error(command);
@@ -79,15 +79,7 @@ public final class TaskList implements Runnable {
         new SetDone(toDoList, out).setDone(idString, false);
     }
 
-    private void help() {
-        out.println("Commands:");
-        out.println("  show");
-        out.println("  add project <project name>");
-        out.println("  add task <project name> <task description>");
-        out.println("  check <task ID>");
-        out.println("  uncheck <task ID>");
-        out.println();
-    }
+
 
     private void error(String command) {
         out.printf("I don't know what the command \"%s\" is.", command);
