595db70cdf20fa6fa5e65fc48644a4cfdb6a122c
Teddy <teddy.chen.tw@gmail.com>
2024-04-17 16:15:24 +0800

    [Step 4-2: Command Use Case] Implement AddTaskUseCase, including AddTaskInput, AddTaskService, and inline Add to Execute

--- src/main/java/tw/teddysoft/tasks/usecase/Execute.java
+++ src/main/java/tw/teddysoft/tasks/usecase/Execute.java
@@ -2,6 +2,13 @@ package tw.teddysoft.tasks.usecase;
 
 import tw.teddysoft.tasks.entity.ToDoList;
 import tw.teddysoft.tasks.usecase.port.out.ToDoListRepository;
+import tw.teddysoft.tasks.usecase.port.in.project.add.AddProjectInput;
+import tw.teddysoft.tasks.usecase.port.in.project.add.AddProjectUseCase;
+import tw.teddysoft.tasks.TaskList;
+import tw.teddysoft.tasks.usecase.service.AddProjectService;
+import tw.teddysoft.tasks.usecase.port.in.task.add.AddTaskUseCase;
+import tw.teddysoft.tasks.usecase.port.in.task.add.AddTaskInput;
+import tw.teddysoft.tasks.usecase.service.AddTaskService;
 
 import java.io.PrintWriter;
 
@@ -25,7 +32,7 @@ public class Execute {
                 new Show(toDoList, out).show();
                 break;
             case "add":
-                new Add(toDoList, out, repository).add(commandRest[1]);
+                add(commandRest[1]);
                 break;
             case "check":
                 check(commandRest[1]);
@@ -39,6 +46,27 @@ public class Execute {
             default:
                 new Error(out).error(command);
                 break;
+        }
+    }
+
+    private void add(String commandLine) {
+        String[] subcommandRest = commandLine.split(" ", 2);
+        String subcommand = subcommandRest[0];
+        if (subcommand.equals("project")) {
+            AddProjectUseCase addProjectUseCase = new AddProjectService(repository);
+            AddProjectInput addProjectInput = new AddProjectInput();
+            addProjectInput.toDoListId = TaskList.DEFAULT_TO_DO_LIST_ID;
+            addProjectInput.projectName = subcommandRest[1];
+            addProjectUseCase.execute(addProjectInput);
+        } else if (subcommand.equals("task")) {
+            String[] projectTask = subcommandRest[1].split(" ", 2);
+            AddTaskUseCase addTaskUseCase = new AddTaskService(repository);
+            AddTaskInput addTaskInput = new AddTaskInput();
+            addTaskInput.toDoListId = TaskList.DEFAULT_TO_DO_LIST_ID;
+            addTaskInput.projectName = projectTask[0];
+            addTaskInput.description = projectTask[1];
+            addTaskInput.done = false;
+            out.print(addTaskUseCase.execute(addTaskInput).getMessage());
         }
     }
 
